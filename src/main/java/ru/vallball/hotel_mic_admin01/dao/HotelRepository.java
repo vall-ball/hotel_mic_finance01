627ea06bcf335dfb41ede077b8ef169b4043b9e8
vall-ball <vall-ball@yandex.ru>
2021-02-19 11:52:53 +0700

    the commit

--- src/main/java/ru/vallball/hotel_mic_admin01/dao/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.vallball.hotel_mic_admin01.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ru.vallball.hotel_mic_admin01.model.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, Long>{
-
-}
--- src/main/java/ru/vallball/hotel_mic_admin01/dao/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.vallball.hotel_mic_admin01.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ru.vallball.hotel_mic_admin01.model.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long>{
-
-}
